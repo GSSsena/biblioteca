71eebcaf99969e8ce6e15dfa8f40f34e99cb3cb2
Guilherme <guilhermedasilvasena960@gmail.com>
2026-02-27 15:46:26 -0300

    base do projeto

--- item_bilbioteca.py
+++ item_bilbioteca.py
@@ -17,3 +17,31 @@ class ItemBiblioteca:
             else:
                 print("Deve ter um Titulo.")
 
+        def get_ano(self):
+            return self.__ano
+
+        def set_ano(self, ano):
+            if ano > 0:
+                self.__ano = ano
+            else:
+                print("O Ano deve ser maior que 0.")
+
+        def is_disponivel(self):
+            return self.__disponivel
+
+        def emprestar(self):
+            if self.__disponivel:
+                self.__disponivel = False
+                prnt(f"{self.__titulo} livro emprestado!")
+            else:
+                print(f"{self.__titulo} não tem está disponivel.")
+
+        def devolver(self):
+            if not self.__disponivel:
+                self.__disponivel = True
+                print(f"{self.__titulo} livro devolvido!")
+            else:
+                print(f"{self.__titulo} livro está disponivel.")
+
+        def exibir_detalhes(self):
+            print(f"Codigo: {self.__codigo}, Titulo: {self.__titulo}, Ano: {self.__ano}, Disponivel: {self.__disponivel}")
