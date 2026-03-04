268b32eeface529ae8be8acc6e8ccbaa656b85ae
Guilherme <guilhermedasilvasena960@gmail.com>
2026-03-04 09:52:38 -0300

    fiz a  classe Revista herdando ItemBiblioteca

--- revista.py
+++ revista.py
@@ -1,1 +1,18 @@
 from item_bilbioteca import ItemBiblioteca
+
+class Revista(ItemBilioteca):
+    def __init__(self, codigo, titulo, ano, edição, mes):
+        super().__init__(codigo, titulo, ano)
+        self.__edição = edição
+        se.__mes = mes
+
+    def exibir_detalhes(self):
+        status = "Disponivel" if self.get_disponivel() else "Emprestado"
+        print(f"[Revista] codigo: {self.get_codigo()} | "
+        f"titulo: {self.get_titulo()} | "
+        f"Edição: {self.__edicao} | "
+        f"Mês: {self.__mes} | "
+        f"Ano: {self.get_ano()} | "
+        f"Status: {status}")
+        
+    
